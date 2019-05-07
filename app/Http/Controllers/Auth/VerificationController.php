aa76d150eb6fc7d0d57b2c182d309adf825f73cb
stefan018 <mladenovic.stefan1997@gmail.com>
2019-05-07 14:39:49 +0200

    test  v2

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/prijava';
 
     /**
      * Create a new controller instance.
