bbf8bc5465d3cd5ab1b2913e1a7822c784a3054a
stefan018 <mladenovic.stefan1997@gmail.com>
2019-05-24 10:34:25 +0200

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -55,6 +55,7 @@ class HomeController extends Controller
 
     public function store(Request $request)
     {
+      
         if(Auth::user()->student==true)
         {
           $input=Input::only('name','last_name','email','phoneNumber');
