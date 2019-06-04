4b7c2606add8a1441171a82db284037f9c231040
stefan018 <mladenovic.stefan1997@gmail.com>
2019-06-04 11:51:01 +0200

    test 2

--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,5 @@ Route::post('search', 'PublicController@search')->name('search');
 Route::post('/dodajOglas', 'adUploadController@addStudentAd')->name('StudentAd');
 
 Route::get('/publisherReview', 'PublicController@publisherReview')->name('publisherReview');
+
 Route::get('/studentReview', 'PublicController@studentReview')->name('studentReview');
