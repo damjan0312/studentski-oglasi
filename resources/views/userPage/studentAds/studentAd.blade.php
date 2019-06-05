169225dfc15a19c9a082f1238ce81a02cda24832
damjan0312 <mladenovic0312@gmail.com>
2019-06-05 13:12:02 +0200

    xzv

--- resources/views/userPage/studentAds/studentAd.blade.php
+++ resources/views/userPage/studentAds/studentAd.blade.php
@@ -65,22 +65,6 @@
 
                     </div><!-- kraj kartice -->
 
-                    <div class="col-md-3 text-center">
-                    <div class="ad p-3">   <!-- pocetak kartice -->
-
-                                    <div class="card p-2">
-
-                                    <div class="card-block">
-                                    <h4 class="card-title font-weight-bold">Trazim cimera</h4>
-                                    <hr>
-                                    <p class="card-text">Stan na bulevaru, 45 kvadrata sa terasom.</p>
-                                    <a  id="ad-linkk" href="#">Pogledaj oglas</a>
-                                    </div>
-                                     </div>
-                        </div>
-
-                    </div><!-- kraj kartice -->
-</div>
 
 
 
